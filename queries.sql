81e3188e7635e9bd179a31a26e8ccee9978948b1
Fardin Ahmed <fardin.cse25@gmail.com>
2026-06-14 12:15:43 +0600

    first two queries done

--- queries.sql
+++ queries.sql
@@ -132,6 +132,34 @@ VALUES
     (505, 3, 102, 'C-20', 'Pending', 120.00);
 
 
-    -- Queries :
-    select match_id, fixture, base_ticket_price from 
-    
+
+-- Queries :
+select match_id, fixture, base_ticket_price from matches
+where tournament_category = 'Champions League' and match_status = 'Available'
+
+
+select user_id, full_name, email from users
+where full_name ilike 'Tanvir%' or full_name ilike '%Haque'
+
+
+select booking_id, user_id, match_id, coalesce(payment_status, 'Action Required') as systematic_status from bookings
+where payment_status is null
+
+
+select booking_id, full_name, fixture, total_cost from bookings
+inner join users using(user_id)
+inner join matches using(match_id)
+
+
+select user_id, full_name, booking_id from bookings
+full join users using(user_id)
+
+
+select booking_id, match_id, total_cost from bookings
+where total_cost > (select avg(total_cost) from bookings)
+
+
+select match_id, fixture, base_ticket_price from matches
+order by base_ticket_price desc
+offset 1
+limit 2
